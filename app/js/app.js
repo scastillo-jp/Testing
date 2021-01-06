12230f79b7d7a68b32f183da393951cb74cd0861
Vanessa <vanessainiguez98@gmail.com>
2021-01-06 01:13:17 -0500

    TU-5 Check date format

--- app/js/app.js
+++ app/js/app.js
@@ -3,4 +3,5 @@ const testingAngluarApp = angular.module('testingAngularApp', []);
 testingAngluarApp.controller('testingAngularCtrl', function($scope) {
     $scope.title = 'Testing AngularJS Applications';
     $scope.message = 'Holaa Mundo';
+    const date = new Date();
 });
--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -4,6 +4,7 @@ describe('Testing AngularJS Test Suite', function () {
    describe('Testing AngularJS Controller', function () {
     var scope = {};
     var ctrl;
+    
 
     beforeEach(
       inject(function ($controller) {
@@ -19,6 +20,29 @@ describe('Testing AngularJS Test Suite', function () {
       expect(scope.message).toBeDefined();
       expect(scope.message.length).toBe(11);
     });
+
+
+    it('should happen if the format is day month year', function() {
+      const currentDate = new Date();
+      const currentDay = currentDate.getDate();
+      const currentMonth = currentDate.getMonth() + 1;
+      const currentYear = currentDate.getFullYear();
+
+      const formatDate = currentDay + "/" + currentMonth +  "/" + currentYear;
+
+      const formatDateArray = formatDate.split('/');
+
+      console.log(formatDateArray);
+
+      const ifCorrectDay = parseInt(formatDateArray[0]) > 31 ? true : false;
+
+      const ifCorrectMonth = parseInt(formatDateArray[1]) > 12 ? true : false;
+
+      expect(ifCorrectDay).toEqual(false);
+      expect(ifCorrectMonth).toEqual(false);
+    });
+
+
   });
 
 });
